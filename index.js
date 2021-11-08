e6d50c472c8c7ca39a32d5c2d774dae080abfc93
seanlandsman <sean@thelandsmans.com>
2021-11-08 12:45:23 +0000

    Update dependencies

--- index.js
+++
@@ -1,4 +0,0 @@
-import AgGridApp from './AgGridApp.vue'
-
-Vue.component('ag-grid-app', AgGridApp);
-
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    chainWebpack: config => {
-        config.externals({
-            'ag-grid-community': 'ag-grid-vue',
-            'ag-grid-vue': 'ag-grid-vue'
-        })
-    }
-}
